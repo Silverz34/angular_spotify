b432386aa04bd63f4abb386f957f51098d4cd6a2
Silverz34 <pradogordillomichael@gmail.com>
2025-10-22 22:55:10 -0600

    cookiesservice modificado

--- src/app/services/spotify-api/spotify-login-service.ts
+++ src/app/services/spotify-api/spotify-login-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environments } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +15,10 @@ export class SpotifyLoginService {
   getToken(): Observable<any> {
     const body = new HttpParams()
       .set("grant_type", "client_credentials")
-      .set("client_id", environments.CLIENT_ID)
-      .set("client_secret", environments.CLIENT_SECRET);
+      .set("client_id", environment.CLIENT_ID)
+      .set("client_secret", environment.CLIENT_SECRET);
     return this._http.post<any>(
-      environments.AUTH_API_URL,
+      environment.AUTH_API_URL, 
       body.toString(),
       {
         headers: {'Content-Type': "application/x-www-form-urlencoded"}
--- src/app/services/spotify-api/spotify-playlist-service.ts
+++ src/app/services/spotify-api/spotify-playlist-service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SpotifyPlaylistResponse } from '../../interfaces/spotify-api/spotify-playlist-response';
-import { CookieStorageService } from '../cookie-storage-service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +9,13 @@ import { CookieStorageService } from '../cookie-storage-service';
 export class SpotifyPlaylistService {
 
   constructor(
-    private _http: HttpClient,
-    private _cookieStorageService: CookieStorageService
+    private _http: HttpClient
   ){ }
 
   getPlaylist(): Observable<SpotifyPlaylistResponse>{
 
     return this._http.get<SpotifyPlaylistResponse>(
-      "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n",
-      {
-        headers: {
-          'Authorization': "Bearer " + this._cookieStorageService.getCookieValue('access_token')
-        }
-      }
+      "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n"
     )
 
   }
