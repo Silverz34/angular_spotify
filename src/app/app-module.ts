ea6b03fa0efdf5634804bb4e6f03211398de39c3
Silverz34 <pradogordillomichael@gmail.com>
2025-10-23 21:09:06 -0600

    Cambio en la IU

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -10,6 +10,7 @@ import { Player } from './player/player';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth-interceptor';
 import { addAuthHeaderInterceptor } from './interceptors/add-auth-header-interceptor';
+import { Headebar } from './headebar/headebar';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { addAuthHeaderInterceptor } from './interceptors/add-auth-header-interce
     SongInfo,
     AudioController,
     Playlist,
-    Player
+    Player,
+    Headebar
   ],
   imports: [
     BrowserModule,
